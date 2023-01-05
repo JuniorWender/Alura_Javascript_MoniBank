65a47b6ebdeed36bb4f465ed770efefd92436f40
wender.junior <wender.junior@etaure.com.br>
2023-01-05 09:23:14 -0300

    Add cpf validator

--- js/verify-cpf.js
+++ js/verify-cpf.js
@@ -1,6 +1,11 @@
 export default function IsCPF(field) {
     const cpf = field.value.replace(/\.|-/g,"");
-    verifyRepitedNumberField(cpf);
+    if(verifyRepitedNumber(cpf) || !verifyFirstNumber(cpf) || !verifySecondNumber(cpf)){
+        console.log("CPF Inválido!");
+    }else{
+        console.log("CPF Válido!");
+    }
+
 }
 
 function verifyRepitedNumber(cpf) {
@@ -14,8 +19,44 @@ function verifyRepitedNumber(cpf) {
         '66666666666',
         '77777777777',
         '88888888888',
-        '99999999999',
+        '99999999999'
     ]
 
     return repitedNumbers.includes(cpf);
+}
+
+function verifyFirstNumber(cpf) {
+    let sum = 0;
+    let mult = 10;
+
+    for (let lenght = 0; lenght < 9; lenght++) {
+        sum += cpf[length] * mult;
+        mult --;
+    }
+
+    sum = (sum * 10) % 11;
+
+    if(sum == 10 || sum == 11){
+        sum = 0;
+    }
+    console.log('primeiro',sum);
+    return sum != cpf[9];
+}
+
+function verifySecondNumber(cpf) {
+    let sum = 0;
+    let mult = 11;
+
+    for (let lenght = 0; lenght < 10; lenght++) {
+        sum += cpf[length] * mult;
+        mult --;
+    }
+
+    sum = (sum * 10) % 11;
+
+    if(sum == 10 || sum == 11){
+        sum = 0;
+    }
+    console.log('segundo',sum);
+    return sum != cpf[10];
 }
